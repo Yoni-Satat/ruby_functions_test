541219965f372b2a8544a96bc793bac2b9dee472
Yoni Satat <yoni.satat@gmail.com>
2017-09-26 16:56:03 +0100

    passed ninth test

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -11,11 +11,11 @@ def subtract(first_number, second_number)
 end
 
 def multiply(first_number, second_number)
-  return 4 * 2
+  return first_number * second_number
 end
 
 def divide(first_number, second_number)
-  return 10 / 2
+  return first_number / second_number
 end
 
 def length_of_string(s1)
@@ -28,4 +28,15 @@ end
 
 def add_string_as_number(str_num_1, str_num_2)
   return str_num_1.to_i + str_num_2.to_i
+end
+
+def number_to_full_month_name(num)
+  case num
+    when 1
+      "January"
+    when 3
+      "March"
+    when 9
+      "September"
+  end
 end
